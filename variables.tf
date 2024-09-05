22de63577aa5c6fb2c764e4d60cebbc8c9ab3978
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-09-05 16:28:10 +0530

    terraform-vpc

---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "cidr_block" {
+    default = {}
+}
+
+variable "enable_dns_support" {
+    default = true
+}
+
+variable "enable_dns_hostnames" {
+    default = true
+}
+
+variable "common_tags" {
+    default = {}
+    type = map
+}
+
+variable "vpc_tags" {
+    default = {}
+    type = map
+}
+
+variable "igw_tags" {
+    default = {}
+}
+
+variable "public_subnet_cidr" {
+  
+}
+
+variable "azs" {
+  
+}
+
+variable "public_subnet_names" {
+  
+}
+
+variable "private_subnet_cidr" {
+  
+}
+
+variable "private_subnet_names" {
+  
+}
+
+variable "data_base_subnet_cidr" {
+  
+}
+
+variable "data_base_subnet_names" {
+  
+}
